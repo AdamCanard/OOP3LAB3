f49f667d9a6e21a7056c079983dcd99bf96e6c72
jovielarue <44442031+jovielarue@users.noreply.github.com>
2024-10-24 13:28:59 -0600

    Do My Part

--- src/implementations/Dictionary.java
+++ src/implementations/Dictionary.java
@@ -1,83 +1,89 @@
 package implementations;
 
 import java.util.ArrayList;
-
 import utilities.DictionaryADT;
 
 public class Dictionary<K, V> implements DictionaryADT<K, V> {
-	// constant
-	private static final int DEFAULT_SIZE = 10;
+  // constant
+  private static final int DEFAULT_SIZE = 10;
 
-	// index indicates pairing, e.g. keys[1] is stored at values[1]
-	private ArrayList<K> keys;
-	private ArrayList<V> values;
+  // index indicates pairing, e.g. keys[1] is stored at values[1]
+  private ArrayList<K> keys;
+  private ArrayList<V> values;
 
-	@Override
-	public V insert(K key, V value) {
-		if (this.containsKey(key)) {
-			return this.update(key, value);
-		} else {
-			keys.add(key);
-			values.add(value);
-			return null;
-		}
+  @Override
+  public V insert(K key, V value) {
+    if (this.containsKey(key)) {
+      return this.update(key, value);
+    } else {
+      keys.add(key);
+      values.add(value);
+      return null;
+    }
+  }
 
-	}
+  @Override
+  public V remove(K key) {
+    int index = 0;
+    for (K currentKey : keys) {
+      if (currentKey.equals(key)) {
+        V valueToReturn = this.lookup(currentKey);
+        keys.remove(index);
+        values.remove(index);
+        return valueToReturn;
+      }
+      index++;
+    }
+    return null;
+  }
 
-	@Override
-	public V remove(K key) {
-		for (K currentKey : keys) {
-			if (currentKey.equals(key)) {
-				K keyToReturn = currentKey;
-				currentKey = null;
-				return keyToReturn;
-			}
-		}
-		// Jovie
-		return null;
-	}
+  @Override
+  public V update(K key, V value) {
+    if (this.containsKey(key)) {
 
-	@Override
-	public V update(K key, V value) {
-		if (this.containsKey(key)) {
+      V oldValue = this.lookup(key);
 
-			V oldValue = this.lookup(key);
+      int counter = 0;
+      for (K currentKey : keys) {
+        if (currentKey.equals(key)) {
+          values.set(counter, value);
+          return oldValue;
+        }
+        counter++;
+      }
+    }
+    return null;
+  }
 
-			int counter = 0;
-			for (K currentKey : keys) {
-				if (currentKey.equals(key)) {
-					values.set(counter, value);
-					return oldValue;
-				}
-				counter++;
-			}
+  @Override
+  public V lookup(K key) {
+    int index = 0;
+    for (K currentKey : keys) {
+      if (currentKey.equals(key)) {
+        return values.get(index);
+      }
+      index++;
+    }
+    return null;
+  }
 
-		}
-		return null;
+  @Override
+  public boolean containsKey(K key) {
+    for (K currentKey : keys) {
+      if (currentKey.equals(key)) {
+        return true;
+      }
+    }
+    return false;
+  }
 
-	}
-
-	@Override
-	public V lookup(K key) {
-		// Jovie
-		return null;
-	}
-
-	@Override
-	public boolean containsKey(K key) {
-		for (K currentKey : keys) {
-			if (currentKey.equals(key)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	@Override
-	public boolean containsValue(V value) {
-		// Jovie
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+  @Override
+  public boolean containsValue(V value) {
+    for (V currentValue : values) {
+      if (currentValue.equals(value)) {
+        return true;
+      }
+    }
+    return false;
+  }
 }
